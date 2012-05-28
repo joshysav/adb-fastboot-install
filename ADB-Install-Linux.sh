5e2ab1be452db33da7c43be57916f5464e1a94a8
Kyle Laker <10kylelaker@gmail.com>
2012-05-28 15:49:10 -0400

    initial project version

---
+++ ADB-Install-Linux.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#  ADB Install.sh
+#  
+#  This script installs adb and fastboot into /bin/
+#  This may be run from anywhere and is compatible with Linux
+#  and Mac OS X. Hopefully a Windows batch file will be available
+#  very soon.
+#
+#  Created by kalaker on 04/20/2012
+#  Updated by kalaker on 04/30/2012
+#  http://forum.xda-developers.com/member.php?u=4510103 <-My XDA Profile
+echo "This will install ADB and Fastboot on your computer."
+echo "Root Permissions required. Please type your password."
+sudo cd ~ #Gives Superuser permissions
+cd "`dirname "$0"`" #CDs to script directory
+echo "Installing JDK (Not really necessary, but helpful for future)."
+sudo add-apt-repository ppa:sun-java-community-team/sun-java6; sudo apt-get update; sudo apt-get install sun-java6-jre sun-java6-bin sun-java6-jdk
+echo "JDK Installed"
+echo "Changed directory to `pwd`" #Informs user of path change
+echo "Moving ADB"
+sudo mv Linux/adb_Linux /bin/adb #Moves adb
+echo "ADB Moved to /bin/adb"
+echo "moving Fastboot"
+sudo mv Linux/fastboot_Linux /bin/fastboot #Moves Fastboot
+echo "Fastboot moved to /bin/fastboot"
+echo "Installing rules"
+sudo mv Linux/51-android.rules /etc/udev/rules.d/51-android.rules #Installs rules
+echo "Rules installed"
+echo "You may now run Android Debug Bridge and Fastboot commands"
+echo "Have a nice day."
